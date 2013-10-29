61c8131d0582899e3de2f1cb34356045e2b1e628
Anonymous <none>
2013-10-29 01:39:36 -0700

    can now dump names using kanji too

--- dumpshort.py
+++ dumpshort.py
@@ -1,9 +1,8 @@
 from sys import argv
 from utils import ints2int, hexify
-from shortutils import byte2str
 
 
-def get_messages(infile, tableaddress):
+def get_messages(infile, tableaddress, charsize=1):
     pointers = []
     bank = tableaddress & 0xffff8000
     infile.seek(tableaddress)
@@ -18,10 +17,11 @@ def get_messages(infile, tableaddress):
         infile.seek(pointer)
         message = []
         while True:
-            data = ord(infile.read(1))
-            if data == 0:
+            data = map(ord, infile.read(charsize))
+            if data[0] == 0:
                 break
             else:
+                data = ints2int(data, bigend=False)
                 message.append(data)
         messages.append(message)
 
@@ -31,7 +31,14 @@ def get_messages(infile, tableaddress):
 if __name__ == "__main__":
     infile = open(argv[1], 'rb')
     address = int(argv[2], 16)
-    messages = get_messages(infile, address)
+    charsize = int(argv[3]) if len(argv) > 3 else 1
+    messages = get_messages(infile, address, charsize=charsize)
+
+    if charsize == 1:
+        from shortutils import byte2str
+    else:
+        from utils import byte2str
+
     for m in messages:
         m = "".join(map(lambda c: byte2str[c] if c in byte2str else '*', m))
         print m
